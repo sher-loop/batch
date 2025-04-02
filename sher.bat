4e3d0eb89fc197522edb89925a4d56fc7f1d2d2f
sher-loop <surendhar.fin@gmail.com>
2025-04-02 09:43:00 +0530

    Add files via upload

---
+++ sher.bat
@@ -0,0 +1,30 @@
+@echo off
+:menu
+cls
+echo ============================
+echo   Hey Sherlock......!
+echo ============================
+echo 1. Open Winmonitor
+echo 2. Close Winmonitor
+echo 3. Exit
+echo ============================
+set /p choice="Enter your choice : "
+
+if "%choice%"=="1" goto Winmonitorop
+if "%choice%"=="2" goto Winmonitorcls
+if "%choice%"=="3" exit
+echo Invalid choice! Try again.
+pause
+goto menu
+
+:Winmonitorop
+start "" "C:\Users\17705\Downloads\winmonitor-common.exe" %1
+echo Application opened successfully.
+pause
+goto menu
+
+:Winmonitorcls
+taskkill /f /im winmonitor-common.exe
+echo Application closed successfully.
+pause
+goto menu
