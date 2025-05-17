052a4774395c2c0ff8496efe3e9d1771d571d40a
sher-loop <surendhar.fin@gmail.com>
2025-05-17 07:44:22 +0530

    Update link.bat

--- link.bat
+++ link.bat
@@ -1,14 +1,10 @@
 @echo off
 set /p link=link:
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
-start %link%
+set /p time=H M T:
+
+set times=%time%
+
+for /l %%i in (1,1,%times%) do (
+    start "" %link%
+)
+
