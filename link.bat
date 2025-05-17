13e5f0356bb3e75429c9a84b0c68f9950cf246d1
sher-loop <surendhar.fin@gmail.com>
2025-05-17 07:49:17 +0530

    Update link.bat

--- link.bat
+++ link.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /p link=Enter link (e.g. https://example.com): 
-set /p count=How many times to open: 
+set /p link=link: 
+set /p count=H M T: 
 
 for /l %%i in (1,1,!count!) do (
     start "" "!link!"
